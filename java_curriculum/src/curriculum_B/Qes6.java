0aac8f82ccc0a20be2c067728d48b836e543658c
kanik <kanik@nt>
2023-05-19 22:05:47 +0900

    sample提出

--- java_curriculum/src/curriculum_B/Qes6.java
+++ java_curriculum/src/curriculum_B/Qes6.java
@@ -1,17 +1,38 @@
 package curriculum_B;
 
-import java.util.Random;
+import java.util.Scanner;
 
 public class Qes6 {
 
 	public static void main(String[] args) {
 		
-
+		Scanner sc = new Scanner(System.in);
+		String getName = sc.next();
+		String[] names = getName.split("、");
+		
+		for (String name : names) {
+			System.out.println(name);
+		}
 	}
 	
-	public static int mNum () {
-		Random rand = new Random();
-		return rand.nextInt(11);
-	}
+//	Scanner scanner = new Scanner(System.in);
+//	scanner.useDelimiter("、");
+//	String name = scanner.next();
+	
+
+	
+//	public static int mNum () {
+//		Random rand = new Random();
+//		return rand.nextInt(12);
+//	}
+	
+	
+	
+	//-----------------後で消す-------------------
+	//----------------使いそうなやつメモ----------
+	//splitメソッド
+	//https://style.potepan.com/articles/32906.html#i-2
+	//→splitメソッドは、対象となる文字列を指定した区切り文字によって分割し、
+	//それぞれを配列の要素として格納します
 
 }
--- java_curriculum/src/sample/SelfIntroduction.java
+++ java_curriculum/src/sample/SelfIntroduction.java
@@ -3,17 +3,16 @@ package sample;
 public class SelfIntroduction {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-		//氏名	
-		String name = "nonezumi";
-		//年齢
-		int age = 3;
-		//趣味
-		String hobby = "kakurenbo";
+		// 氏名
+		String name = "山田　花子";
+		// 年齢
+		int age = 25;
+		// 趣味
+		String hobby = "映画鑑賞";
 		
 		/*
-		 * 自己紹介文を出力する処理
-		 * */
+		 * 	自己紹介を出力する処理
+		 */
 		
 		System.out.println("私の名前は" + name + "です。");
 		System.out.println("年齢は" + age + "歳です。");
