fdbf4ef140910bf04932cbec9e9a28517abb3f1e
rio <7noah13@gmail.com>
2023-06-03 21:14:06 +0900

    提出06/03

--- java_curriculum/src/curriculum_B/Qes1_3.java
+++ java_curriculum/src/curriculum_B/Qes1_3.java
@@ -1,5 +1,6 @@
 package curriculum_B;
 
+//import java.util.Objects;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.regex.Pattern;
@@ -8,91 +9,172 @@ public class Qes1_3 {
 
 	public static void main(String[] args) {
 		
-		
-		
-		// Scannerインスタンスを生成し、標準入力を取得
-		Scanner scanner = new Scanner(System.in);
-		// キーボード入力した内容の「改行までの1行分」を取得
-		String name = scanner.nextLine();
-		// nameの値が有効かどうか判定する
-		if (Pattern.matches("^[^A-Za-z0-9]+$", name)) {
-			System.out.println("半角英数字のみで名前を入力してください");
+		// 入力された名前を格納する変数
+		String name = "";
+				
+		while (!((Pattern.matches("[A-Za-z0-9]+$", name)) && (name.length() < 10)) ) {
+			
+			// コンソールにお願いを出力
+			System.out.println("ユーザー名を入力してください");
+			// Scannerインスタンスを生成し、標準入力を取得
+			Scanner scanner = new Scanner(System.in);
+			// キーボード入力した内容の「改行までの1行分」を取得
+			name = scanner.nextLine();
+			
+			// nameの値が有効(半角英数字)かどうか判定する
+			if (Pattern.matches("[^A-Za-z0-9]+$", name)) {
+				
+				// コンソールにお願いを出力
+				System.out.println("半角英数字のみで名前を入力してください");
+			
+			}
+			// ユーザーネームが10文字を超えた時の処理
+			else if (name.length() >= 10) {
+				
+				// コンソールにお願いを出力
+				System.out.println("名前を10文字以内にしてください");
+			
+			
+			} 
+			// ユーザーネームが入力されなかった際の処理
+			else if (name.length() == 0 || name == null) {
+			
+				// コンソールにお願いを出力
+				System.out.println("名前を入力してください");
+			
+			
+			} 
+			// 上の条件に当てはまらなかった際の処理
+			else {
+				
+				// コンソールにユーザーネーム登録完了の旨を出力
+				System.out.println("\nユーザー名「" + name +  "」を登録しました");
+				// コンソールにお願いを出力
+				System.out.print("\nじゃんけんの手を入力してください" ) ;
+				// じゃんけんの処理を呼び出し
+				judgeJanken(name);
+				
+			}	
 		}
-		else if (name.length() > 10) {
-			System.out.println("名前を10文字以内にしてください");
-		}else if (name.length() == 0) {
-			System.out.println("名前を入力してください");
-		}else {
-			System.out.println("ユーザー名「" + name +  "」を登録しました");
-			
-			judgeJanken(name);
-			
-		}	
 	}
-
+	/****************************
+	 * pcのじゃんけんの手をランダムで生成する
+	 ****************************/
 	public static int  getNpc() {
-		Random rand = new Random();
-		return rand.nextInt(3);	
-	}
-	
-	public static int getUser() {
-		Scanner scannerNum = new Scanner(System.in);
 		
-		while(true) {
-		// 入力されたデータが整数かどうかのチェック
-		if (scannerNum.hasNextInt()) {
-			// 入力されたデータを整数として読み込む
-			int userNum = scannerNum.nextInt();
-			if(userNum >= 0 &&  userNum <= 2) {
-				// 0,1,2の場合、メソッドの結果として返す
-				return userNum;
-			}else {
-				scannerNum.next();
-			}
-		}else {
-			scannerNum.next();
-		}
-		}
+		// ランダムインスタンスを作成
+		Random rand = new Random();
+		//0～2までの数字を呼び出しもとに返す
+		return rand.nextInt(3);	
+	
 	}
 	
-	public static void judgeJanken(String name) { 
-		int counter = 1;
-		String[] janken = {"グー","チョキ","パー"};
-			while(true) {
-				int user = getUser();
-				int npc = getNpc();
-				if ((user == 0 && npc == 1) || (user == 1 && npc == 2) || (user == 2 && npc == 0)) {
-					System.out.println(name + "の手は「" + janken[user] + "」");
-					System.out.println("相手の手は「" + janken[npc] + "」");
-					System.out.println("次は俺にリベンジさせて\n");
-					System.out.println("勝つまでにかかった回数は" + counter + "回です");
-					break;
-				}else if ((user == 0 && npc == 2)) {
-					System.out.println(name + "の手は「" + janken[user] + "」");
-					System.out.println("相手の手は「" + janken[npc] + "」\n");
-					System.out.println("俺の勝ち\n負けは次につながるチャンスです！/nネバーギブアップ！");
-					counter ++;
-
-				}else if ((user == 1 && npc == 0)) {
-					System.out.println(name + "の手は「" + janken[user] + "」");
-					System.out.println("相手の手は「" + janken[npc] + "」\n");
-					System.out.println("俺の勝ち！\nたかがじゃんけん、そう思ってないですか？\nそれやったら次も、俺が勝ちますよ");
-					counter ++;
-				}else if ((user == 2 && npc == 1)) {
-					System.out.println(name + "の手は「" + janken[user] + "」");
-					System.out.println("相手の手は「" + janken[npc] + "」\n");
-					System.out.println("俺の勝ち！\nなんで負けたか、明日まで考えといてください。\nそしたら何かが見えてくるはずです");
-					counter ++;
-				}else {
-					System.out.println(name + "の手は「" + janken[user] + "」");
-					System.out.println("相手の手は「" + janken[npc] + "」\n");
-					System.out.println("DRAW あいこ もう一回しましょう！");
-					counter ++;
+	
+	/****************************
+	 * 入力されたじゃんけんの値が有効な値になるまで値を受け取る
+	 ****************************/
+	public static int getUser() {
+		
+		// user空うけっとた値が正しい場合数字を格納する変数　※初期化の際0だと都合が悪いため、好きな数字を入れました
+		int returnNum = 8;
+		
+		// 正しい値を受け取るまで入力を繰り返す
+		while (!(0 <= returnNum && returnNum <= 2) ) {
+			
+			// コンソールに入力するためスキャナーインスタンスを作成
+			Scanner scannerNum = new Scanner(System.in);
+			// スキャナーで受け取った値を変数に格納
+			String userNum = scannerNum.nextLine();
+			
+				// 受け取った値が0,1,2の時の処理
+				if(Pattern.matches("[0-2]", userNum)) {
+					
+					// 0～2以外の文字を空白に置き換え
+					String intStr = userNum.replaceAll("[^0-2]", "");
+					// 文字から数値に変換して変数に格納
+					returnNum = Integer.parseInt(intStr);
+					
 				}
-			}
+				// それ以外の場合の処理
+				else {
+					
+					System.out.println("文字が入っている、改行または0～2じゃないよ");
+					
+				}
+				
 		}
-	
+		// 受け取った値を返す
+		return returnNum;
+		
 	}
+	
 
+	
+	
+	/****************************
+	 * じゃんけんの勝ち負けを判定する処理
+	 ****************************/
+	public static void judgeJanken(String name) { 
+		
+		// じゃんけんの手を初期化
+		int user = 0;
+		// じゃんけんの手を初期化
+		int npc = 0;
+		
+		// 勝つまでにかかった回数を受け取る変数
+		int counter = 1;
+		
+		// じゃんけんの手を配列に格納
+		String[] janken = {"グー","チョキ","パー"};
+		
+		// じゃんけんのコメントを配列に格納
+		String[] coment = {"俺の勝ち\n負けは次につながるチャンスです！\nネバーギブアップ！","俺の勝ち！\nたかがじゃんけん、そう思ってないですか？\nそれやったら次も、俺が勝ちますよ","俺の勝ち！\nなんで負けたか、明日まで考えといてください。\nそしたら何かが見えてくるはずです"};
+			
+		// 勝つまでじゃんけんの処理を繰り返す
+		while(!((user == 0 && npc == 1) || (user == 1 && npc == 2) || (user == 2 && npc == 0))) {
+			
+			// ユーザーの手を受け取る
+			user = getUser();
+			// pcでランダムに生成された手を受け取る
+			npc = getNpc();
+			
+			// ユーザーの手をコンソールに出力
+			System.out.println("\n" + name + "の手は「" + janken[user] + "」");
+			// pcの手をコンソールに出力
+			System.out.println("相手の手は「" + janken[npc] + "」\n");
+			
+			// じゃんけんに勝った時の処理
+			if ((user == 0 && npc == 1) || (user == 1 && npc == 2) || (user == 2 && npc == 0)) {
+				
+				// コンソールにメッセージを出力
+				System.out.println("次は俺にリベンジさせて\n");
+				// 勝つまでにかかった回数をコンソールに出力
+				System.out.println("勝つまでにかかった回数は" + counter + "回です");
+
+			}
+			// 自分がじゃんけんでグーで負けた場合の処理
+			else if ((user == 0 && npc == 2) || (user == 1 && npc == 0) || (user == 2 && npc == 1)) {
+				
+				// コンソールにメッセージを出力
+				System.out.println(coment[user]);
+				// じゃんけんした回数を増やす
+				counter ++;
+				
+			}
+			// あいこの時の処理
+			else {
+				
+				// コンソールにメッセージを出力
+				System.out.println("DRAW あいこ もう一回しましょう！");
+				// じゃんけんした回数を増やす
+				counter ++;
+				
+			}
+			
+		}
+			
+	}
+	
+}
 
 
--- java_curriculum/src/curriculum_B/Qes6.java
+++ java_curriculum/src/curriculum_B/Qes6.java
@@ -1,17 +1,79 @@
 package curriculum_B;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Qes6 {
 
 	public static void main(String[] args) {
 		
+		// スキャナーインスタンスを作成
+		Scanner sc = new Scanner(System.in);
+		
+		// コンソールで受け取った値を、で区切って配列で受け取る
+		String[] names = sc.next().split("、");
+		
+		// テレビの個数を格納する変数
+		int tv = rNum();
+		// ディスプレイの数を格納する変数
+		int dp = 11 - tv;
+		
 
+		// 拡張for文で配列に受け取った値の在庫数を出力する処理
+		for (String name : names) {
+			
+			// 値ごとの処理
+			switch (name) {
+			
+			 	// 値がパソコンの時の処理
+				case "パソコン":
+				
+				// 値が冷蔵庫の時の処理
+				case "冷蔵庫":
+					
+				// 値が扇風機の時の処理
+				case "扇風機":
+					
+				// 値が洗濯機の時の処理
+				case "洗濯機":
+					
+				// 値が加湿器の時の処理
+				case "加湿器":
+					
+					// 入力された家電のの在庫数をコンソールに出力
+					System.out.println(name + "の残りの台数は"+ rNum() + "台です" + "\n");
+					break;
+
+				// 値がテレビの時とディスプレイの時の処理
+				case "テレビ": 
+				case "ディスプレイ":
+					// 受け取った値によって処理を分ける
+					int num = name.equals( "テレビ") ? tv : dp;
+					// 入力された家電のの在庫数をコンソールに出力
+					System.out.println(name + "の残りの台数は"+ num  + "台です" + "\n");
+					break;
+					
+				// その他の場合の処理
+				default:
+					// コンソールにコメントを出力
+					System.out.println("『 " + name + " 』は指定の商品ではありません");
+			
+			}
+			
+		}
+		
 	}
 	
-	public static int mNum () {
+	/****************************
+	 * 在庫数を0～11までの数字でランダムで生成
+	 ****************************/
+	public static int rNum () {
+		
+		// ランダムインスタンスを生成
 		Random rand = new Random();
-		return rand.nextInt(11);
+		// ランダムで生成した値を呼び出しもとに返す
+		return rand.nextInt(12);
+		
 	}
 
 }
