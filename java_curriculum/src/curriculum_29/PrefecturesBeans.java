bfcce5027e120f2606ac4374077fc6119caed15e
rio <7noah13@gmail.com>
2023-07-14 23:57:34 +0900

    提出0714

--- java_curriculum/src/curriculum_29/PrefecturesBeans.java
+++ java_curriculum/src/curriculum_29/PrefecturesBeans.java
@@ -7,60 +7,51 @@ import java.util.Map;
 public class PrefecturesBeans implements Serializable{
 
 	// Mapの宣言
-    private Map<Integer, String> prefe = new HashMap<Integer, String>();
-		
+	private Map<Integer, String> prefe = new HashMap<Integer, String>();
+
 	// コンストラクタ
 	public PrefecturesBeans() {
-        
-        // Mapにデータを格納
-		// 北海道
-        prefe.put( 0, "北海道:札幌市:83424");
-        // 青森
-        prefe.put( 1, "青森県:青森市:9646");
-        // 岩手
-        prefe.put( 2, "岩手県:盛岡市:15275");
-        // 宮城
-        prefe.put( 3,"宮城県:仙台市:7282");
-        // 秋田
-        prefe.put( 4, "秋田県:秋田市:11638");
-        // 山形
-        prefe.put( 5, "山形県:山形市:9323");
-        // 福島
-        prefe.put( 6, "福島県:福島市:13784");
-        // 茨木
-        prefe.put( 7, "茨城県:水戸市:6097");
-        // 栃木
-        prefe.put( 8, "栃木県:宇都宮市:6408");
-        // 群馬
-        prefe.put( 9, "群馬県:前橋市:6362");
-        // 埼玉
-        prefe.put( 10, "埼玉県:さいたま市:3798");
-		
-	}
-		
-		// メモ
-		// コンソールで受け取る→nullチェック→okなら配列に,区切りで数字を格納
-		// 昇順、降順をコンソールで受け取る→nullチェック→okなら受け取った数字を指示通りに並べ替える
-		// 並び変えたとおりにコンソールに都道府県情報を表示
-		// keyで呼び出しvalueの値をtemp[]に:区切りで仮格納する
-		// その配列を使って指示通りにコンソールに都道府県情報を表示する
-		// 重複があった場合に削除するメソッドを
-		
-		// 都道府県の情報を変数にセットする処理
-		public void setPref(Integer key, String value) {
-			
-			// 都道府県の情報を変数に代入 
-			prefe.put( key, value);
-		
-		}
-       
-	   // 都道府県の情報を変数から取得する処理
-        public String getPref(Integer num) {
-        
-        	//メソッド呼び出しもとに都道府県の変数の値を渡す
-        	return prefe.get(num);
-        	
-        }
 
+		// Mapにデータを格納
+		// 北海道
+		prefe.put( 0, "北海道:札幌市:83424");
+		// 青森
+		prefe.put( 1, "青森県:青森市:9646");
+		// 岩手
+		prefe.put( 2, "岩手県:盛岡市:15275");
+		// 宮城
+		prefe.put( 3,"宮城県:仙台市:7282");
+		// 秋田
+		prefe.put( 4, "秋田県:秋田市:11638");
+		// 山形
+		prefe.put( 5, "山形県:山形市:9323");
+		// 福島
+		prefe.put( 6, "福島県:福島市:13784");
+		// 茨木
+		prefe.put( 7, "茨城県:水戸市:6097");
+		// 栃木
+		prefe.put( 8, "栃木県:宇都宮市:6408");
+		// 群馬
+		prefe.put( 9, "群馬県:前橋市:6362");
+		// 埼玉
+		prefe.put( 10, "埼玉県:さいたま市:3798");
+
+	}
+
+	// 都道府県の情報を変数にセットする処理
+	public void setPref(Integer key, String value) {
+
+		// 都道府県の情報を変数に代入 
+		prefe.put( key, value);
+
+	}
+
+	// 都道府県の情報を変数から取得する処理
+	public String getPref(Integer num) {
+
+		//メソッド呼び出しもとに都道府県の変数の値を渡す
+		return prefe.get(num);
+
+	}
 
 }
--- java_curriculum/src/curriculum_29_main/Main.java
+++ java_curriculum/src/curriculum_29_main/Main.java
@@ -7,38 +7,9 @@ import curriculum_29.Sort;
 public class Main {
 
 	public static void main(String[] args) {
-		// 参考サイト
-		
-		// 配列を一度リストに変換してから検索
-		// https://magazine.techacademy.jp/magazine/22166
-		// https://sejuku.net/blog/14166
-		// https://style.potepan.com/articles/27478.html#Javalist
-		
-		// ソート
-		// https://www.javadrive.jp/start/array/index14.html
-		
-		// Map(連想配列)
-		// https://sejuku.net/blog/16055
-		
-		// 連想配列でソート
-		// https://sejuku.net/blog/16176
-		// https://www.techiedelight.com/ja/sort-map-by-keys-java/
-		
-		// エラー処理
-		// https://detail.chiebukuro.yahoo.co.jp/qa/question_detail/q11220144656
-		// https://detail.chiebukuro.yahoo.co.jp/qa/question_detail/q13184099648
-		// https://uxmilk.jp/48686
-		
-		// 配列型変換
-		// https://www.javadrive.jp/start/array/index2.html#section1
-		
-		// 正規表現
-		// https://www-creators.com/archives/5154#i-2
-		
-		// 重複削除
-		
+
 		/*
-	 	
+
 	 	0, "北海道:札幌市:83424"
 		1, "青森県:青森市:9646"
 		2, "岩手県:盛岡市:15275"
@@ -50,20 +21,20 @@ public class Main {
 		8, "栃木県:宇都宮市:6408"
 		9, "群馬県:前橋市:6362"
 		10, "埼玉県:さいたま市:3798"
-	 	
+
 	 	コンソールに数字と昇順・降順の値が入力されると上記配列に対応する
 	 	都道府県がソートされてコンソールに出力されるように作ってください
 	   	※Packageを2つ作ってください
 	   	※複数選択できるようにしてください
-	   	
+
 	   	例:
 	   	8,5,9,.....
 	   	と入力された場合（昇順）
-	   	
+
 	   	都道府県名：山形県
 		県庁所在地：山形市
 		面積：9323.0km2
-	   	
+
 	   	都道府県名：栃木県
 		県庁所在地：宇都宮市
 		面積：6408.0km2
@@ -71,8 +42,8 @@ public class Main {
 	   	都道府県名：群馬県
 		県庁所在地：前橋市
 		面積：6362.0km2
-		
-	 */
+
+		 */
 		// 都道府県並べ替えクラスのインスタンスを作成
 		Sort sort = new Sort();
 		// コンソールで値を受け取るためのスキャナー
