29719c508c27b14472fc6a85e67e8ebfbe5a307b
rio <7noah13@gmail.com>
2023-07-15 22:50:18 +0900

    自己紹介プログラム１コメント追記　提出07/15

--- java_curriculum/src/selfIntroduction_1/Main.java
+++ java_curriculum/src/selfIntroduction_1/Main.java
@@ -1,18 +1,23 @@
 package selfIntroduction_1;
 
 class Main {
-	
+
 	// https://engineer-curriculum.com/%E3%83%AC%E3%83%83%E3%82%B9%E3%83%B3/1-31-%e8%87%aa%e5%b7%b1%e7%b4%b9%e4%bb%8b%e3%83%97%e3%83%ad%e3%82%b0%e3%83%a9%e3%83%a0%e3%82%92%e4%bd%9c%e6%88%90%e2%91%a0-2/
-	
+
 	public static void main(String[] args) {
-		
+
+		// 自己紹介情報を持ったインスタンスを生成
 		Person person1 = new Person("鈴木太郎", 20, 1.7, 60);
 
+		// 名前を出力
 		System.out.println(person1.name);
+		// 年齢を出力
 		System.out.println(person1.age);
+		// 身長を出力
 		System.out.println(person1.height);
-		
+
+		// 自己紹介を出力するメソッドを呼び出し
 		person1.print();
-		
+
 	}
 }
--- java_curriculum/src/selfIntroduction_1/Person.java
+++ java_curriculum/src/selfIntroduction_1/Person.java
@@ -2,37 +2,53 @@ package selfIntroduction_1;
 
 public class Person{
 	// インスタンスフィールドを定義
+	// 名前を格納する変数
 	String name;
+	// 年齢を格納する変数
 	int age;
+	// 身長を格納する変数
 	double height;
+	// 体重を格納する変数
 	double weight;
-	double bmi;
+	// 人数を格納する変数
 	static int count = 0;
-	
+
 	// コンストラクタを定義しインスタンスフィールドに値をセット
 	public Person(String name, int age, double height, double weight){
-		
+
+		// 変数に名前を代入
 		this.name = name;
+		// 変数に年齢を代入
 		this.age = age;
+		// 変数に身長を代入
 		this.height = height;
+		// 変数に体重を代入
 		this.weight = weight;
+		// インスタンスを生成するたびに人数をカウントする
 		count++;
-		
+
 	}
-	
+
+	// bmiを計算するメソッド
 	public double bmi() {
-		
-		 return weight/(height*height);
-		
+
+		// メソッド呼び出しもとにBMIの値を返す
+		return weight/(height*height);
+
 	}
-	
+
+	// 自己紹介の内容をコンソールに出力する処理
 	public void print() {
-		
+
+		// 名前を出力
 		System.out.println("\n名前は" + this.name + "です");
+		// 年齢を出力
 		System.out.println("年は" + this.age  + "才です");
+		// bmiを出力
 		System.out.println("BMIは" + bmi() + "です");
+		// 人数の合計を出力
 		System.out.println("合計" + count + "人です");
-		
+
 	}
-	
+
 }
